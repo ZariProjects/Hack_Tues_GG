f8131be6a6305cb306ce068367a33b30823d1579
GeorgiY <rey_mysterio11@abv.bg>
2021-03-12 12:47:05 +0200

    Card.js file changes for the CSS

--- green_goal/src/components/Cart/Card.js
+++ green_goal/src/components/Cart/Card.js
@@ -2,22 +2,17 @@ import React from 'react';
 import './Card.css'
 
 const card = (props) => {
-    let certification = props.isCertified ? <p>GreenGoal Certified</p> : null;
-    const imgWidth = 75;
-    const imgHeigth = 75;
+    let certification = props.isCertified ? <p className = "hr_text"> GreenGoal Certified</p> : null;
     return (
-        <div className="Card" onClick={props.clicked}>
-            <div className="Img">
-            <   img src={props.img} alt={props.productName} width={imgWidth} height={imgHeigth} />
-            </div>
-            <div className="Text">
-                <p>{props.productName}</p>
-                <p>{props.sellerName}</p>
-                <p>{props.rating}/10</p>
-                <p>{props.price}lv</p>
-                {certification}
-            </div>
-            
+        <div className = "Border" onClick={props.clicked}>
+            <img src={props.img} alt={props.productName} className = "Img" />
+            <div className = "Text">
+                <p className = "hr_text">{props.productName}</p>
+                <p className = "hr_text">{props.sellerName}</p>
+                <p className = "hr_text">{props.rating}/10</p>
+                <p className = "hr_text">{props.price}lv</p>
+                {certification}      
+            </div>      
         </div>
     );
 };
