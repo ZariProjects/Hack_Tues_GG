c2c159b04a53895ff116dbcb608e16f2ac43197a
stefan0413 <48565659+stefan0413@users.noreply.github.com>
2021-03-12 03:35:15 +0200

    complete navbar
    От към бекенд всичко му е готово, остава да се направи фронтенд и ще е топ

--- green_goal/src/components/NavigationBar/NavigationBar.js
+++ green_goal/src/components/NavigationBar/NavigationBar.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import {MenuItems} from "./MenuItems"
+import {MenuItems} from './MenuItems'
+import { Button } from '../Button/Button'
 import './NavigationBar.css'
 
 class Navbar extends Component {
+    state = { clicked: false}
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render() {
         return(
             <nav className = "NavbarItems">
                 <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon">
-                    
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item,index) => {
                         return (
                             <li key = {index}>
@@ -23,6 +30,7 @@ class Navbar extends Component {
                         )
                     })} 
                 </ul>
+                <Button> Settings</Button>
             </nav>
         )
     }
