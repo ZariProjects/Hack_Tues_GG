6b6aa1fb496accbc3a1eadcd6c5d945ba48c9781
Lacho <61554888+ZariProjects@users.noreply.github.com>
2021-03-12 02:02:26 +0200

    Update App.js

--- green_goal/src/containers/App.js
+++ green_goal/src/containers/App.js
@@ -1,13 +1,18 @@
+import React, { Component } from 'react';
 import './App.css';
 import Carts from './Carts/Carts';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>GreenGoal </h1>
-      <Carts />
-    </div>
-  );
-}
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>GreenGoal</h1>
+        <h2>NavBar ?</h2>
+        <Carts />
+        
+      </div>
+    );
+  }
+} 
 
 export default App;
