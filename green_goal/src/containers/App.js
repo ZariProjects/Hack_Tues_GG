7a743f015a892f464a1738bf8c1daf6620cd1456
Lacho <61554888+ZariProjects@users.noreply.github.com>
2021-03-12 22:06:59 +0200

    Firebase DB Init

--- green_goal/src/containers/App.js
+++ green_goal/src/containers/App.js
@@ -13,6 +13,16 @@ import Login from './Login/Login';
 
 class App extends Component {
   render () {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBVrtDmXuWf_1soIcgkMHGqtyK0OAfCmCg",
+      authDomain: "green-goal-5234f.firebaseapp.com",
+      projectId: "green-goal-5234f",
+      storageBucket: "green-goal-5234f.appspot.com",
+      messagingSenderId: "402826671794",
+      appId: "1:402826671794:web:b704c25a1217445d7c3bab",
+      measurementId: "G-JBGCZGYQ0Q"
+    };
+
     return (
       <div className="App">
         <Navbar/>
@@ -26,7 +36,9 @@ class App extends Component {
           <Route path="/sign_up" component={SignUp} />
           <Route path="/settings" component={Settings}/>
           <Route path="/" exact component={Cards} />
+          {firebaseConfig}
         </Switch >
+        
         
       </div>
     );
