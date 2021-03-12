195ff688618344e57f18c5048d3f3e1b963568ea
stefan0413 <48565659+stefan0413@users.noreply.github.com>
2021-03-12 19:45:25 +0200

    Adding SignUp.js

--- green_goal/src/containers/SignUp/SignUp.js
+++ green_goal/src/containers/SignUp/SignUp.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import './SignUp.css';
+import { NavLink } from 'react-router-dom';
 
 class SignUp extends Component {
     render () {
         return (
-            <div className="SignIn">
-                <h1>Sign Up?</h1>
+            <div class="center">
+                <div class="container">
+                    <div class="text">
+                        Sign Up</div>
+                    <form action="#">
+                        <div class="data">
+                            <label>Email or Phone</label>
+                            <input type="text" required></input>
+                            <i class="far fa-envelope"></i>
+                        </div>
+                        <div class="data">
+                            <label>Password</label>
+                            <input type="password" required></input>
+
+                            <label>Confirm Password</label>
+                            <input type="password" required></input>
+                            <div class="btn">
+                                <div class="inner"></div>
+                                <button type="submit"><span>Sign In</span></button>
+                            </div>
+                            <div class="signup-link">
+                                Already a member?
+                                <NavLink to="/log_in">Login now</NavLink>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
