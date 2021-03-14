a5d85c0abe2991aebb11d1cda5f94bbc9c4927a4
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2021-03-14 02:23:36 +0200

    Home page

--- green_goal/src/containers/Home/Home.js
+++ green_goal/src/containers/Home/Home.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import "./Home.css"
 
 class Home extends Component {
 
     render () {
-        return (
-            <div>
-                <h1>Welcome! </h1>
+        return ( 
+            <div className="containerHome">
+                <div className="whitesquare">
+                  <div className="textHome">
+                    Welcome!</div>
+                    <div className="contentHome">
+                        <p>Our site is a platform that connects producers of organic products with buyers and offers discounts in exchange for ecological challenges</p>
+                        <h3>Help us to safe the world!</h3>
+                    </div>  
+                </div>
             </div>
         );
     }
