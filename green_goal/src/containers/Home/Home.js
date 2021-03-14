3913a5b6ccbec8f85b56b7f9603428802df9604e
yavor <yavor_mi005@mail.bg>
2021-03-14 03:05:24 +0200

    Cart.js init

--- green_goal/src/containers/Home/Home.js
+++ green_goal/src/containers/Home/Home.js
@@ -4,16 +4,18 @@ import "./Home.css"
 
 class Home extends Component {
 
-    render () {
-        return ( 
-            <div className="containerHome">
-                <div className="whitesquare">
-                  <div className="textHome">
-                    Welcome!</div>
-                    <div className="contentHome">
-                        <p>Our site is a platform that connects producers of organic products with buyers and offers discounts in exchange for ecological challenges</p>
-                        <h3>Help us to safe the world!</h3>
-                    </div>  
+    render() {
+        return (
+            <div className="HomeBody">
+                <div className="containerHome">
+                    <div className="whitesquare">
+                        <div className="textHome">
+                            Welcome!</div>
+                        <div className="contentHome">
+                            <p>Our site is a platform that connects producers of organic products with buyers and offers discounts in exchange for ecological challenges</p>
+                            <h3>Help us to safe the world!</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
@@ -27,4 +29,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(Home); 
+export default connect(mapStateToProps)(Home);
